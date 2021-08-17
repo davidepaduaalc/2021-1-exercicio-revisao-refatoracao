d7955c4588621198a4f5a2eca43c57cc8fd15bd9
davidepaduaalc <89022374+davidepaduaalc@users.noreply.github.com>
2021-08-17 15:22:00 -0300

    Update Apartamento.hpp

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -10,20 +10,6 @@ class Apartamento : public Imovel {
 
     public: 
     
-    double valor() {
-
-        double v = AREA * VALORm2;
-
-        return v;
-
-    }
-
-    double comissao() {
-
-        return valor() * porcentagem;
-        
-    }
-
     void print() {
 
         std::cout << "[Apartamento]" << endl;
